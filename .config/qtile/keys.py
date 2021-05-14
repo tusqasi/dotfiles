870240e665d41e162f210f778908ef7377f7304a
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-05-14 14:48:38 +0530

    Working state of qtile

--- .config/qtile/keys.py
+++ .config/qtile/keys.py
@@ -1,7 +1,7 @@
 from libqtile.config import Key
 from libqtile.command import lazy
 from group import groups
-from variables import *
+from variables import mod, alt, terminal, browser, launcher
 
 keys = [
     # Switch between windows
@@ -9,96 +9,90 @@ keys = [
     Key([mod], "l", lazy.layout.right(), desc="Move focus to right"),
     Key([mod], "j", lazy.layout.down(), desc="Move focus down"),
     Key([mod], "k", lazy.layout.up(), desc="Move focus up"),
-    Key([mod], "space", lazy.layout.next(),
-        desc="Move window focus to other window"),
-
+    Key([mod], "space", lazy.layout.next(), desc="Move window focus to other window"),
     # Move windows between left/right columns or move up/down in current stack.
     # Moving out of range in Columns layout will create new column.
-    Key([mod, "shift"], "h", lazy.layout.shuffle_left(),
-        desc="Move window to the left"),
-    Key([mod, "shift"], "l", lazy.layout.shuffle_right(),
-        desc="Move window to the right"),
-    Key([mod, "shift"], "j", lazy.layout.shuffle_down(),
-        desc="Move window down"),
+    Key(
+        [mod, "shift"], "h", lazy.layout.shuffle_left(), desc="Move window to the left"
+    ),
+    Key(
+        [mod, "shift"],
+        "l",
+        lazy.layout.shuffle_right(),
+        desc="Move window to the right",
+    ),
+    Key([mod, "shift"], "j", lazy.layout.shuffle_down(), desc="Move window down"),
     Key([mod, "shift"], "k", lazy.layout.shuffle_up(), desc="Move window up"),
-
     # Grow windows. If current window is on the edge of screen and direction
     # will be to screen edge - window would shrink.
-    Key([mod, "control"], "h", lazy.layout.grow_left(),
-        desc="Grow window to the left"),
-    Key([mod, "control"], "l", lazy.layout.grow_right(),
-        desc="Grow window to the right"),
-    Key([mod, "control"], "j", lazy.layout.grow_down(),
-        desc="Grow window down"),
+    Key([mod, "control"], "h", lazy.layout.grow_left(), desc="Grow window to the left"),
+    Key(
+        [mod, "control"], "l", lazy.layout.grow_right(), desc="Grow window to the right"
+    ),
+    Key([mod, "control"], "j", lazy.layout.grow_down(), desc="Grow window down"),
     Key([mod, "control"], "k", lazy.layout.grow_up(), desc="Grow window up"),
     Key([mod], "n", lazy.layout.normalize(), desc="Reset all window sizes"),
-
     # Toggle between split and unsplit sides of stack.
     # Split = all windows displayed
     # Unsplit = 1 window displayed, like Max layout, but still with
     # multiple stack panes
-    Key([mod, "shift"], "Return", lazy.layout.toggle_split(),
-        desc="Toggle between split and unsplit sides of stack"),
+    Key(
+        [mod, "shift"],
+        "Return",
+        lazy.layout.toggle_split(),
+        desc="Toggle between split and unsplit sides of stack",
+    ),
+    # Launch stuff #
     Key([mod], "Return", lazy.spawn(f"{terminal} -e fish"), desc="Launch terminal"),
     # Browswer
     Key([mod], "o", lazy.spawn(browser), desc="Launch browser"),
+    Key([], "Print", lazy.spawn("flameshot gui"), desc="Launch flameshot"),
+    Key(["shift"], "Print", lazy.spawn("gcolor3"), desc="Launch gcolor3 picker"),
+    Key([mod, alt],"d", lazy.spawn("discord"), desc="Launch Discord"),
+    Key([mod, "shift"],"space", lazy.spawn(launcher), desc="Launch launcher"),
 
-    Key([],"Print" ,lazy.spawn("flameshot gui"), desc="Launch flameshot"),
-    Key(["shift"],"Print" ,lazy.spawn("gcolor3"), desc="Launch colorPicker"),
+    # Launch stuff end #
     # Toggle between different layouts as defined below
     Key([mod], "Tab", lazy.next_layout(), desc="Toggle between layouts"),
     Key([mod], "w", lazy.window.kill(), desc="Kill focused window"),
-
     # Make floating
-    Key([mod, "shift"],'f',lazy.window.toggle_floating()),
+    Key([mod, "shift"], "f", lazy.window.toggle_floating()),
     Key([mod, "control"], "r", lazy.restart(), desc="Restart Qtile"),
-    Key([mod], "r", lazy.spawncmd(),
-        desc="Spawn a command using a prompt widget"),
+    Key([mod], "r", lazy.spawncmd(), desc="Spawn a command using a prompt widget"),
     # Volume and media
-    Key(
-        [], "XF86AudioRaiseVolume",
-        lazy.spawn("pactl -- set-sink-volume 0 +5%")
-    ),
-    Key(
-        [], "XF86AudioLowerVolume",
-        lazy.spawn("pactl -- set-sink-volume 0 -5%")
-    ),
-    Key(
-        [], "XF86AudioMute",
-        lazy.spawn("pactl set-sink-mute 0 toggle")
-    ),
-
-    Key(
-        [], "XF86AudioPlay",
-        lazy.spawn("playerctl play-pause")
-    ),
-    Key(
-        [], "XF86AudioPause",
-        lazy.spawn("playerctl play-pause")
-    ),
-    Key(
-        [], "XF86AudioNext",
-        lazy.spawn("playerctl next")
-    ),
-    Key(
-        [], "XF86AudioPrev",
-        lazy.spawn("playerctl previous")
-    ),
+    Key([], "XF86AudioRaiseVolume", lazy.spawn("pactl -- set-sink-volume 0 +5%")),
+    Key([], "XF86AudioLowerVolume", lazy.spawn("pactl -- set-sink-volume 0 -5%")),
+    Key([], "XF86AudioMute", lazy.spawn("pactl set-sink-mute 0 toggle")),
+    Key([], "XF86AudioPlay", lazy.spawn("playerctl play-pause")),
+    Key([], "XF86AudioPause", lazy.spawn("playerctl play-pause")),
+    Key([], "XF86AudioNext", lazy.spawn("playerctl next")),
+    Key([], "XF86AudioPrev", lazy.spawn("playerctl previous")),
     # Volume and media end
-    
 ]
 for i in groups:
-    keys.extend([
-        # mod1 + letter of group = switch to group
-        Key([mod], i.name, lazy.group[i.name].toscreen(),
-            desc="Switch to group {}".format(i.name)),
-
-        # mod1 + alt + letter of group = switch to & move focused window to group
-        Key([mod, alt], i.name, lazy.window.togroup(i.name, switch_group=True),
-            desc="Switch to & move focused window to group {}".format(i.name)),
-
-        # Or, use below if you prefer not to switch to that group.
-        # # mod1 + shift + letter of group = move focused window to group
-        Key([mod, "shift"], i.name, lazy.window.togroup(i.name),
-            desc="move focused window to group {}".format(i.name)),
-    ])
+    keys.extend(
+        [
+            # mod1 + letter of group = switch to group
+            Key(
+                [mod],
+                i.name,
+                lazy.group[i.name].toscreen(),
+                desc="Switch to group {}".format(i.name),
+            ),
+            # mod1 + alt + letter of group = switch to & move focused window to group
+            Key(
+                [mod, alt],
+                i.name,
+                lazy.window.togroup(i.name, switch_group=True),
+                desc="Switch to & move focused window to group {}".format(i.name),
+            ),
+            # Or, use below if you prefer not to switch to that group.
+            # # mod1 + shift + letter of group = move focused window to group
+            Key(
+                [mod, "shift"],
+                i.name,
+                lazy.window.togroup(i.name),
+                desc="move focused window to group {}".format(i.name),
+            ),
+        ]
+    )
--- .config/qtile/layout.py
+++ .config/qtile/layout.py
@@ -2,20 +2,20 @@ from libqtile import layout
 from libqtile.config import Match
 
 
-# layout_default = {
-#         'border_focus': '#675d55',
-#         'border_normal':'#fe4cbb3',
-#         'border_focus_stack': '#881111',
-#         'margin': 5,
-#         'margin_on_single': 0,
-#         }
+layout_default = {
+        'border_focus': '#675d55',
+        # 'border_normal':'#fe4cbb3',
+        'border_focus_stack': '#881111',
+        'margin': 15,
+        'margin_on_single': 1,
+        }
 layouts = [
-    layout.Columns(
-        # border_normal="#fe4cbb3",
-        border_focus_stack='#fe4cbb3',
-        ),
+    layout.MonadTall(**layout_default),
     layout.Max(),
-    # layout.MonadTall(**layout_default),
+    # layout.Columns(
+    #     # border_normal="#fe4cbb3",
+    #     border_focus_stack='#fe4cbb3',
+    #     ),
     # Try more layouts by unleashing below layouts.
     # layout.Stack(num_stacks=2),
     # layout.Bsp(),
