1bb6a363b418592cd61ad99b71c86b61241458fd
tusqasi <2002.tushar.kuntawar@gmail.com>
2021-10-20 22:52:15 +0530

    fish prt correction

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,9 +1,4 @@
 function fish_prompt
-    if set -q VIRTUAL_ENV
-	echo -n -s (set_color blue) "(" (basename "$VIRTUAL_ENV") ")" (set_color normal) " "
-	echo 	
-    end
-
     if test -n "$SSH_TTY"
         echo -n (set_color brred)"$USER"(set_color white)'@'(set_color yellow)(prompt_hostname)' '
     end
@@ -11,7 +6,7 @@ function fish_prompt
     echo -n (set_color blue)(prompt_pwd)' '
 
     set_color -o
-    if fish_is_root_user
+    if test "$USER" = 'root'
         echo -n (set_color red)'# '
     end
     echo -n (set_color red)'❯'(set_color yellow)'❯'(set_color green)'❯ '
