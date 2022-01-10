b28a32cd5112babf1e3d5b810b96028fb0a662e3
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-01-10 12:19:13 +0530

    nice

--- .config/qtile/config.py
+++ .config/qtile/config.py
@@ -251,7 +251,7 @@ def start_up():
 @hook.subscribe.client_new
 def send_to_www(window):
     windowtype = window.window.get_wm_class()[1]
-    browsers = ["firefox", "brave","chromium", "qutebrowser"]
+    browsers = ["firefox", "brave", "chromium", "qutebrowser"]
     if windowtype.lower() in browsers:
         window.togroup("WWW")
 
