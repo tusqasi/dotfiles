998a60d5dc4be8257f85029addec9bad495dffb6
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-01-30 13:37:59 +0530

    ♻ Formatting and chores

--- .config/nvim/plugin/letnset.lua
+++ .config/nvim/plugin/letnset.lua
@@ -4,22 +4,22 @@ local M = {}
 local cmd = vim.cmd
 
 function M.create_augroup(autocmds, name)
-  cmd("augroup " .. name)
-  cmd("autocmd!")
-  for _, autocmd in ipairs(autocmds) do
-    cmd("autocmd " .. table.concat(autocmd, " "))
-  end
-  cmd("augroup END")
+    cmd("augroup " .. name)
+    cmd("autocmd!")
+    for _, autocmd in ipairs(autocmds) do
+        cmd("autocmd " .. table.concat(autocmd, " "))
+    end
+    cmd("augroup END")
 end
 M.create_augroup(
-  {
     {
-      "BufEnter",
-      "*.html,*.css,*.js",
-      ":EmmetInstall"
-    }
-  },
-  "emmet"
+        {
+            "BufEnter",
+            "*.html,*.css,*.js",
+            ":EmmetInstall"
+        }
+    },
+    "emmet"
 )
 
 vim.opt.mouse = "n"
@@ -34,6 +34,7 @@ vim.opt.foldlevel = 99
 -- Hybrid numbers
 vim.opt.number = true
 
+vim.opt.relativenumber = true
 -- Encoding
 vim.opt.encoding = "utf-8"
 -- Tab to 4
@@ -42,6 +43,24 @@ vim.opt.shiftwidth = 4
 
 vim.api.nvim_command("filetype off")
 
+vim.opt.fillchars = {
+  vert = '▕', -- alternatives │
+  fold = ' ',
+  eob = ' ', -- suppress ~ at EndOfBuffer
+  diff = '╱', -- alternatives = ⣿ ░ ─
+  msgsep = '‾',
+  foldopen = '▾',
+  foldsep = '│',
+  foldclose = '▸',
+}
+
+vim.opt.listchars = {
+  eol = nil,
+  tab = '│ ',
+  extends = '»',
+  precedes = '«',
+  trail = '•',
+}
 -- don't close exited buffer
 vim.opt.hidden = true
 -- Backups and swap and such
@@ -49,7 +68,7 @@ vim.opt.hidden = true
 vim.opt.swapfile = true
 vim.opt.undofile = true
 -- Persistent Undo
-vim.opt.undodir = vim.g.nvim_dir .. "nvim/undodir/"
+vim.opt.undodir = vim.env.HOME .. "/.local/share/nvim/undodir/"
 
 -- Make those folders automatically if they don't already exist.
 -- if vim.fn.isdirectory(vim.o.undodir) == 0 then
@@ -66,34 +85,34 @@ vim.opt.inccommand = "nosplit"
 vim.opt.updatetime = 10
 -- vim.opt.wildignore = vim.opt.wildignore .. "*/tmp/*,*.so,*.swp,*.zip,*.gz*"
 vim.opt.wildignore = {
-  "*.aux",
-  "*.avi",
-  "*.class",
-  "*.dll",
-  "*.gif",
-  "*.gz",
-  "*.ico",
-  "*.jar",
-  "*.jpeg",
-  "*.jpg",
-  "*.o",
-  "*.obj",
-  "*.out",
-  "*.png",
-  "*.pyc",
-  "*.rbc",
-  "*.toc",
-  "*.wav",
-  "*.zip",
-  -- Temp/System
-  "*/tmp/*",
-  "*.*~",
-  "*.swp",
-  "*~ ",
-  ".DS_Store",
-  "*.so",
-  ".lock",
-  "tags.lock"
+    "*.aux",
+    "*.avi",
+    "*.class",
+    "*.dll",
+    "*.gif",
+    "*.gz",
+    "*.ico",
+    "*.jar",
+    "*.jpeg",
+    "*.jpg",
+    "*.o",
+    "*.obj",
+    "*.out",
+    "*.png",
+    "*.pyc",
+    "*.rbc",
+    "*.toc",
+    "*.wav",
+    "*.zip",
+    -- Temp/System
+    "*/tmp/*",
+    "*.*~",
+    "*.swp",
+    "*~ ",
+    ".DS_Store",
+    "*.so",
+    ".lock",
+    "tags.lock"
 }
 
 -- better use bash instead of fish
