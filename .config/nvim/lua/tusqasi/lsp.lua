dd9241dcf715bcea26379e5e5048068b78d7f24f
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-01-30 13:40:48 +0530

    ♻ Minor refactors

--- .config/nvim/lua/tusqasi/lsp.lua
+++ .config/nvim/lua/tusqasi/lsp.lua
@@ -1,38 +1,41 @@
 -- enable lsp plugin
-local nvim_lsp = require "lspconfig"
+local nvim_lsp = require("lspconfig")
 
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
-    -- wrapper functions
-    local function buf_set_keymap(...)
-        vim.api.nvim_buf_set_keymap(bufnr, ...)
-    end
-    -- local function buf_set_option(...)
-    --     vim.api.nvim_buf_set_option(bufnr, ...)
-    -- end
-    --
-    -- Enable completion triggered by <c-x><c-o>
-    -- buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
+  -- wrapper functions
+  local function buf_set_keymap(...)
+    vim.api.nvim_buf_set_keymap(bufnr, ...)
+  end
+  local function buf_set_option(...)
+    vim.api.nvim_buf_set_option(bufnr, ...)
+  end
 
-    -- Mappings.
-    local opts = {noremap = true, silent = true}
+  -- Enable completion triggered by <c-x><c-o>
+  buf_set_option("omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
-    buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-    buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
-    buf_set_keymap("n", "<space>e", "<cmd>lua vim.lsp.diagnostic.show_line_diagnostics()<CR>", opts)
-    buf_set_keymap("n", "<space>K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-    buf_set_keymap("n", "<space>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
-    buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
-    buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
-    buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
-    buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
-    buf_set_keymap("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", opts)
-    buf_set_keymap("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", opts)
-    buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
-    -- buf_set_keymap("n", "<space><CR>", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
+  -- Mappings.
+  local opts = {noremap = true, silent = true}
+
+  -- See `:help vim.lsp.*` for documentation on any of the below functions
+  buf_set_keymap("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
+  buf_set_keymap("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+  buf_set_keymap("n", "K", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+  buf_set_keymap("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
+  buf_set_keymap("n", "<space>k", "<cmd>lua vim.lsp.buf.signature_help()<CR>", opts)
+  -- buf_set_keymap("n", "<space>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>", opts)
+  -- buf_set_keymap("n", "<space>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>", opts)
+  -- buf_set_keymap("n", "<space>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>", opts)
+  buf_set_keymap("n", "<space>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", opts)
+  buf_set_keymap("n", "<space>rn", "<cmd>lua vim.lsp.buf.rename()<CR>", opts)
+  buf_set_keymap("n", "<space>ca", "<cmd>lua vim.lsp.buf.code_action()<CR>", opts)
+  buf_set_keymap("n", "gr", "<cmd>lua vim.lsp.buf.references()<CR>", opts)
+  buf_set_keymap("n", "<space>e", "<cmd>lua vim.diagnostic.open_float()<CR>", opts)
+  buf_set_keymap("n", "[d", "<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>", opts)
+  buf_set_keymap("n", "]d", "<cmd>lua vim.lsp.diagnostic.goto_next()<CR>", opts)
+  buf_set_keymap("n", "<space>q", "<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>", opts)
+  buf_set_keymap("n", "<space><CR>", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
 
 -- used for cmp completions
@@ -45,30 +48,29 @@ capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
 -- install language servers with lsp installer https://github.com/williamboman/nvim-lsp-installer/
 -- ]]
 -- lua
-local sumneko_root_path = "/usr/lib/lua-language-server/bin"
-local sumneko_binary = "/usr/lib/lua-language-server/bin/lua-language-server"
-nvim_lsp.sumneko_lua.setup {
-    cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
-    init_options = {formatting = true},
-    capabilities = capabilities,
-    on_attach = on_attach,
-    settings = {
-        Lua = {
-            diagnostics = {
-                -- declare vim as a global so no warning
-                globals = {
-                    "vim",
-                    "use",
-                    "require"
-                }
-                -- sources = {"nvim_diagnostic"}
-            }
+local system_name = "Linux"
+local sumneko_root_path = "/home/tusqasi/.local/share/nvim/lsp_servers/sumneko_lua/extension/server/"
+local sumneko_binary = sumneko_root_path .. "/bin/" .. system_name .. "/lua-language-server"
+require "lspconfig".sumneko_lua.setup {
+  cmd = {sumneko_binary, "-E", sumneko_root_path .. "/main.lua"},
+  init_options = {formatting = true},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  settings = {
+    Lua = {
+      diagnostics = {
+        -- declare vim as a global so no warning
+        globals = {
+          "vim",
+          "use"
         }
+      }
     }
+  }
 }
 
 -- dart
---[[ nvim_lsp.dartls.setup {
+require "lspconfig".dartls.setup {
   init_options = {formatting = true},
   capabilities = capabilities,
   on_attach = on_attach,
@@ -79,67 +81,32 @@ nvim_lsp.sumneko_lua.setup {
       }
     }
   }
-} ]]
+}
+
 -- java
---[[ nvim_lsp.java_language_server.setup {
+--[[ require "lspconfig".java_language_server.setup {
   cmd = {"/home/tusqasi/clones/java-language-server/dist/launch_linux.sh"},
   root_dir = function(startpath)
     return "./"
   end
 } ]]
 -- java but eclipse
---[[ nvim_lsp.jdtls.setup {
+require "lspconfig".jdtls.setup {
   capabilities = capabilities,
   on_attach = on_attach
-} ]]
--- vimls
---[[ nvim_lsp.vimls.setup {
-  capabilities = capabilities,
-  on_attach = on_attach
-} ]]
+}
+
 -- pylsp
-nvim_lsp.pylsp.setup {
-    capabilities = capabilities,
-    on_attach = on_attach
+require "lspconfig".pylsp.setup {
+  capabilities = capabilities,
+  on_attach = on_attach
 }
--- nvim_lsp.jedi_language_server.setup {
---     capabilities = capabilities,
---     on_attach = on_attach
--- }
-nvim_lsp.cssls.setup {
-    capabilities = capabilities,
-    on_attach = on_attach
+-- vimls
+require "lspconfig".vimls.setup {
+  capabilities = capabilities,
+  on_attach = on_attach
 }
-
-nvim_lsp.tsserver.setup {
-    capabilities = capabilities,
-    on_attach = on_attach
-}
-nvim_lsp.rust_analyzer.setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    root_dir = nvim_lsp.util.root_pattern("Cargo.toml", "rust-project.json")
-}
-
-nvim_lsp.clangd.setup {
-    capabilities = capabilities,
-    on_attach = on_attach
-}
-
-nvim_lsp.clangd.setup {
-    capabilities = capabilities,
-    on_attach = on_attach
-}
-nvim_lsp.ccls.setup {
-    capabilities = capabilities,
-    on_attach = on_attach,
-    init_options = {
-        compilationDatabaseDirectory = "build",
-        index = {
-            threads = 0
-        },
-        clang = {
-            excludeArgs = {"-frounding-math"}
-        }
-    }
+require "lspconfig".tsserver.setup {
+  capabilities = capabilities,
+  on_attach = on_attach
 }
--- .config/nvim/lua/tusqasi/telescope.lua
+++ .config/nvim/lua/tusqasi/telescope.lua
@@ -2,8 +2,9 @@ local telescope = require("telescope")
 
 telescope.setup {
   defaults = {
-    prompt_prefix = "🔍 > ",
-    selection_caret = " ",
+    prompt_prefix = "  > ",
+    selection_caret = "  ",
     entry_prefix = "  "
+    -- file_previewer = require("telescope.previewers").vim_buffer_cat.new
   }
 }
