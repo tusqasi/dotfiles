480eeb0096dbc928cb3408e1117cc5cf97d2e1fc
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-02-11 14:54:43 +0530

    ♻ Change and add keymaps
    - "Y" works as y$ by default from nvim 0.6+
    - Visual drag removed, never really used
    - remove goyo
    - Remove ancient artifact for
    - Change from : to <CMD>
    - Add aerialtoggle

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -1,19 +1,16 @@
 --[[ " keymaps.vim
 -- Does what it says ]]
 --
+--
 local map = vim.api.nvim_set_keymap
 
 -- map the leader key
 local options = {noremap = true}
-map("n", "<space>", "<nop>", {noremap = true, silent = true})
 vim.g.mapleader = " "
+map("n", "<space>", "<nop>", {noremap = true, silent = true})
 
 map("n", ";", ":", options)
 map("n", ":", ";", options)
-
-vim.g.mapleader = " "
--- make Y behave more like C and D
-map("n", "Y", "y$", options)
 
 -- search for open buffers
 map("n", "<Leader>b", "<CMD>lua require('functions').buffers()<CR>", options)
@@ -27,7 +24,7 @@ map("n", "<Leader>R", "<CMD>lua require('functions').registers()<CR>", options)
 -- fd
 map("n", "<Leader>F", "<CMD>lua require('functions').fd()<CR>", options)
 
--- edit nvim
+-- edit nvim configs
 map("n", "<Leader>ne", "<CMD>lua require('functions').nvim_configs()<CR>", options)
 
 -- file broswer
@@ -43,23 +40,12 @@ map("n", "<F2>", ";Vifm<Cr>", options)
 -- <Leader> l to save
 map("n", "<Leader>l", ":update<CR>", {silent = true, noremap = true})
 
--- "visual drag
-map("v", "<LEFT>", "DVB_Drag('left')", {expr = true})
-map("v", "<RIGHT>", "DVB_Drag('right')", {expr = true})
-map("v", "<DOWN>", "DVB_Drag('down')", {expr = true})
-map("v", "<UP>", "DVB_Drag('up')", {expr = true})
-map("v", "D", "DVB_Duplicate()", {expr = true})
--- Distraction Free
-map("n", "<F3>", ";Goyo<Cr>", options)
-
--- "caps = esc
--- au VimEnter * silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Escape'
--- au VimLeave * silent! !xmodmap -e 'clear Lock' -e 'keycode 0x42 = Caps_Lock'
-
 -- Mapping for spell check
-map("n", "<F6>", ";setlocal spell! spelllang=en<CR>", options)
+map("n", "<F6>", "<CMD>setlocal spell! spelllang=en<CR>", options)
 
 -- Diable Help for F1
 map("n", "<F1>", "<NOP>", options)
+
+map("n", "<Leader>o", "<CMD>AerialToggle<CR>", options)
 
 vim.g.user_emmet_leader_key = ","
