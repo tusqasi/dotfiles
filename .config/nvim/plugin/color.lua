01e378871451ffde199cd67bc4c52419f5a1b3e9
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-02-11 14:56:57 +0530

    ♻ Change colorscheme

--- .config/nvim/plugin/color.lua
+++ .config/nvim/plugin/color.lua
@@ -7,5 +7,6 @@
  set termguicolors  ]]
 vim.api.nvim_command("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
 vim.opt.termguicolors = true
-vim.api.nvim_command("colorscheme dracula")
+vim.api.nvim_command("colorscheme gruvbox")
+vim.api.nvim_command("set background=dark")
 vim.api.nvim_command("set termguicolors")
