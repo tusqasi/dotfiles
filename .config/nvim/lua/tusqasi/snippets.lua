1e48367d266a23c9f4aad81cbf94aa7dd11426c2
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-04-01 10:06:45 +0530

    ✨ Snippets in working condition

--- .config/nvim/lua/tusqasi/snippets.lua
+++ .config/nvim/lua/tusqasi/snippets.lua
@@ -4,13 +4,26 @@ local luasnip = require("luasnip")
 local snippet = luasnip.snippet
 local t = luasnip.text_node
 local i = luasnip.insert_node
-local types = require("luasnip.util.types")
+local f = luasnip.function_node
+local d = luasnip.dynamic_node
+local c = luasnip.choice_node
+local snippet_from_nodes = luasnip.sn
+local fmt = require("luasnip.extras.fmt").fmt
 
+local same = function(index)
+    return f(
+        function(args)
+            return args[1]
+        end,
+        {index}
+    )
+end
+local types = require("luasnip.util.types")
 -- Every unspecified option will be set to the default.
 luasnip.config.set_config(
     {
         history = true,
-        updateevents = "TextChanged,TextChangedI",
+        updateevents = "TextChanged,TextChangedI,InsertLeave",
         ext_opts = {
             [types.choiceNode] = {
                 active = {
@@ -20,11 +33,22 @@ luasnip.config.set_config(
         },
         ext_base_prio = 300,
         ext_prio_increase = 1,
-        enable_autosnippets = true
+        enable_autosnippets = false
     }
 )
-
 luasnip.snippets = {
+    lua = {
+        snippet(
+            "use",
+            {
+                f(
+                    function()
+                        return string.format([[use "%s"]], vim.fn.getreg("+"))
+                    end
+                )
+            }
+        )
+    },
     javascriptreact = {
         snippet(
             "cmp",
@@ -78,22 +102,13 @@ luasnip.snippets = {
             }
         )
     },
-    dart = {
-        snippet(
-            "cmp",
-            {
-                t({"export function "}),
-                i(1),
-                t({"() {", "\treturn (", '\t\t<div className="'}),
-                i(2),
-                t({'">'}),
-                i(3),
-                t({"</div>", "\t);", "}"})
-            }
-        )
+    cpp = {
+        snippet("func", fmt([[{} {}({}) {{
+    {};
+}}]], {i(1, {"int"}), i(2), i(3, {"int n"}), i(4)}))
     }
 }
 
 -- add snippet library
--- require("luasnip.loaders.from_vscode").load()
--- require("luasnip").filetype_extend("dart", {"flutter"})
+--
+require("luasnip.loaders.from_vscode").load()
