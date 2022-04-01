5f45b61f7c2cf489657d35226349d3a9b23b0801
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-04-01 10:14:24 +0530

    ✨ Snippets working

--- .config/nvim/lua/tusqasi/cmp.lua
+++ .config/nvim/lua/tusqasi/cmp.lua
@@ -7,42 +7,57 @@ local luasnip = require "luasnip"
 -- nvim-cmp setup
 local cmp = require "cmp"
 cmp.setup {
+    completion = {
+        autocomplete = false
+    },
     snippet = {
         expand = function(args)
-            require("luasnip").lsp_expand(args.body)
+            luasnip.lsp_expand(args.body)
         end
     },
     mapping = {
+        ["<tab>"] = cmp.config.disable,
         ["<C-p>"] = cmp.mapping.select_prev_item(),
         ["<C-n>"] = cmp.mapping.select_next_item(),
         ["<C-d>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = function(fallback)
-            if luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end,
+        ["<C-e>"] = cmp.mapping(
+            function(fallback)
+                if luasnip.expand_or_jumpable() then
+                    luasnip.expand_or_jump()
+                else
+                    fallback()
+                end
+            end,
+            {"i", "s"}
+        ),
         ["<CR>"] = cmp.mapping.confirm {
-            behavior = cmp.ConfirmBehavior.Replace,
+            behavior = cmp.ConfirmBehavior.Insert,
             select = true
         }
     },
     -- sources are in order
     sources = {
+        {name = "luasnip", keyword_length = 2},
         {name = "nvim_lsp", keyword_length = 2},
-        {name = "path", keyword_length = 2},
-        {name = "luasnip", keyword_length = 2}
+        {name = "path", keyword_length = 2}
     }
 }
 
-require "cmp".setup.cmdline(
-    ":",
-    {
-        sources = {
-            {name = "cmdline", keyword_length = 2}
-        }
+cmp.setup.filetype({ 'markdown', 'help' }, {
+    sources = {
+      { name = 'path' },
+      { name = 'buffer' },
+      { name = 'spell' },
     }
-)
+  })
+
+-- require "cmp".setup.cmdline(
+--     ":",
+--     {
+--         sources = {
+--             {name = "cmdline", keyword_length = 2}
+--         }
+--     }
+-- )
