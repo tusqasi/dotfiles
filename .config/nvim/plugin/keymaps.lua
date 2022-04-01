a60c8c6242370fa9beffadbdb5106bedaa0dd756
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-04-01 10:06:04 +0530

    ✨ New telescope keybinds

--- .config/nvim/plugin/keymaps.lua
+++ .config/nvim/plugin/keymaps.lua
@@ -3,6 +3,7 @@
 --
 --
 local map = vim.api.nvim_set_keymap
+local bmap = vim.api.nvim_buf_set_keymap
 
 -- map the leader key
 local options = {noremap = true}
@@ -24,14 +25,26 @@ map("n", "<Leader>R", "<CMD>lua require('functions').registers()<CR>", options)
 -- fd
 map("n", "<Leader>F", "<CMD>lua require('functions').fd()<CR>", options)
 
--- edit nvim configs
+-- Edit nvim configs
 map("n", "<Leader>ne", "<CMD>lua require('functions').nvim_configs()<CR>", options)
+
+-- grep in  nvim configs and edit
+map("n", "<Leader>nE", "<CMD>lua require('functions').grep_nvim_configs()<CR>", options)
 
 -- File broswer
 map("n", "<Leader>Fb", "<CMD>lua require('functions').file_browser()<CR>", options)
 
--- Telescope
-map("n", "<Leader>;", "<CMD>lua require('telescope.builtin').builtin()<CR>", options)
+-- Telescope builtins
+map("n", "<Leader>;", ":Telescope builtin<CR>", options)
+
+-- Spell suggestions at cursor
+map("n", "<Leader>s", ":Telescope spell_suggest<CR>", options)
+
+-- Show code actions at cursor
+map("n", "<Leader>a", ":Telescope lsp_code_actions<CR>", options)
+
+-- Help tags
+map("n", "<Leader>h", ":Telescope help_tags<CR>", options)
 
 -- Yank whole file
 map("n", "<Leader>c", ':norm ggVG"+y<CR>', options)
@@ -39,6 +52,9 @@ map("n", "<F2>", ";Vifm<Cr>", options)
 
 -- <Leader> l to save
 map("n", "<Leader>l", ":update<CR>", {silent = true, noremap = true})
+
+-- Goyo distraction free
+map("n", "<F3>", ":Goyo<CR>", options)
 
 -- Mapping for spell check
 map("n", "<F6>", "<CMD>setlocal spell! spelllang=en<CR>", options)
@@ -49,5 +65,6 @@ map("n", "<F1>", "<NOP>", options)
 map("n", "<Leader>o", "<CMD>AerialToggle<CR>", options)
 
 map("n", "<leader><leader>sn", "<cmd>luafile ~/.config/nvim/lua/tusqasi/snippets.lua<CR>", options)
+map("n", "<leader><leader>x", "<cmd>source %<CR>", options)
 
 vim.g.user_emmet_leader_key = ","
