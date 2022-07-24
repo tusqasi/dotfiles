cb4154f9a16a36eed145bcfe4ff8b645a52131d1
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-07-24 20:51:09 +0530

    ♻ No custom shiftwidths and tabs

--- .config/nvim/ftplugin/lua.lua
+++ .config/nvim/ftplugin/lua.lua
@@ -1,5 +1,5 @@
 -- vim.opt.expandtab = true
-vim.opt.shiftwidth = 2
-vim.opt.tabstop = 2
+-- vim.opt.shiftwidth = 2
+-- vim.opt.tabstop = 2
 -- vim.opt.softtabstop = 4
 -- vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CMD>Neoformat<CR>", {silent = true})
--- .config/nvim/ftplugin/typescriptreact.lua
+++ .config/nvim/ftplugin/typescriptreact.lua
@@ -1,3 +1,3 @@
-vim.opt.shiftwidth = 2
+-- vim.opt.shiftwidth = 2
 -- vim.opt.softtabstop = 2
 -- vim.api.nvim_buf_set_keymap(0, "n", "<CR>", "<CMD>Prettier<CR>", {silent = true})
