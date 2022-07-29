8aebd59900768162554035d315eedd51be95eba1
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-07-29 19:42:22 +0530

    ♻

--- .config/nvim/lua/tusqasi/cmp.lua
+++ .config/nvim/lua/tusqasi/cmp.lua
@@ -2,18 +2,18 @@
 vim.o.completeopt = "menuone,noselect"
 
 -- luasnip setup
-local luasnip = require "luasnip"
+local luasnip = require("luasnip")
 
 -- nvim-cmp setup
-local cmp = require "cmp"
-cmp.setup {
+local cmp = require("cmp")
+cmp.setup({
 	completion = {
-		autocomplete = false
+		autocomplete = false,
 	},
 	snippet = {
 		expand = function(args)
 			luasnip.lsp_expand(args.body)
-		end
+		end,
 	},
 	mapping = {
 		-- ["<tab>"] = cmp.config.disable,
@@ -21,62 +21,46 @@ cmp.setup {
 		["<C-n>"] = cmp.mapping.select_next_item(),
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete({}),
-		["<S-Tab>"] = cmp.mapping(
-			function(fallback)
-				if luasnip.choice_active() then
-					luasnip.change_choice(-1)
-				else
-					fallback()
-				end
-			end),
-		["<Tab>"] = cmp.mapping(
-			function(fallback)
-				if luasnip.choice_active() then
-					luasnip.change_choice(1)
-				else
-					fallback()
-				end
-			end),
-		["<C-e>"] = cmp.mapping(
-			function(fallback)
-				if luasnip.expand_or_jumpable() then
-					luasnip.expand_or_jump()
-				else
-					fallback()
-				end
-			end,
-			{ "i", "s" }
-		),
-		["<CR>"] = cmp.mapping.confirm {
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<S-Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.choice_active() then
+				luasnip.change_choice(-1)
+			else
+				fallback()
+			end
+		end),
+		["<Tab>"] = cmp.mapping(function(fallback)
+			if luasnip.choice_active() then
+				luasnip.change_choice(1)
+			else
+				fallback()
+			end
+		end),
+		["<C-e>"] = cmp.mapping(function(fallback)
+			if luasnip.expand_or_jumpable() then
+				luasnip.expand_or_jump()
+			else
+				fallback()
+			end
+		end, { "i", "s" }),
+		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Insert,
-			select = true
-		}
+			select = true,
+		}),
 	},
 	-- sources are in order
 	sources = {
+
 		{ name = "luasnip", keyword_length = 2 },
 		{ name = "nvim_lsp", keyword_length = 2 },
-		{ name = "path", keyword_length = 2 }
-	}
-}
+		{ name = "path", keyword_length = 2 },
+	},
+})
 
-cmp.setup.filetype(
-	{ "markdown", "help" },
-	{
-		sources = {
-			{ name = "path" },
-			{ name = "buffer" },
-			{ name = "spell" }
-		}
-	}
-)
-
--- require "cmp".setup.cmdline(
---     ":",
---     {
---         sources = {
---             {name = "cmdline", keyword_length = 2}
---         }
---     }
--- )
+cmp.setup.filetype({ "markdown", "help" }, {
+	sources = {
+		{ name = "path" },
+		{ name = "buffer" },
+		{ name = "spell" },
+	},
+})
