98e768552f745ec41c7ec7cb877e49bb8b624c59
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-07-29 19:38:42 +0530

    ✨ Solarized FTW

--- .config/nvim/plugin/color.lua
+++ .config/nvim/plugin/color.lua
@@ -1,6 +1,6 @@
 -- color.vim
 -- vim.api.nvim_command("autocmd VimEnter * hi Normal ctermbg=NONE guibg=NONE")
+-- require("")
 vim.opt.termguicolors = true
-vim.api.nvim_command("colorscheme dracula")
-vim.api.nvim_command("set background=dark")
-vim.api.nvim_command("set termguicolors")
+vim.api.nvim_command("colorscheme NeoSolarized")
+vim.api.nvim_command("set background=light")
