a60123128e859a058d0e85c5fcace05a03aa4257
tusqasi <2002.tushar.kuntawar@gmail.com>
2022-10-15 22:10:54 +0530

    ♻

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,26 +1,18 @@
 vim.g.nvim_dir = vim.fn.expand("~") .. "/.config/nvim"
 
--- vim.g.copilot_enabled = false
-
 Ok, Treesitter = pcall(require, "nvim-treesitter.configs")
 if Ok then
 	Treesitter.setup({ highlight = { enable = true } })
 end
--- Ok, Colorizer = pcall(require, "colorizer")
--- if Ok then
---     vim.opt.termguicolors = true
---     Colorizer.setup({})
--- end
 
--- require("impatient")
+require("impatient")
 require("tusqasi.cmp")
 require("tusqasi.comments")
 require("tusqasi.emmet")
 require("tusqasi.treesitter")
--- require "tusqasi.disable_builtin"
+require "tusqasi.disable_builtin"
 require("tusqasi.formatting")
--- require "tusqasi.globals"
--- require "tusqasi.goyo"
+require "tusqasi.globals"
 require("tusqasi.lsp")
 require("tusqasi.luasnip")
 require("tusqasi.snippets")
