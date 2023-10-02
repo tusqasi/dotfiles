2a86a57636e05af04e370326bd3ab0e244e307b3
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-10-02 10:52:07 +0530

    nvim: make a global for lsp keymap setup consistent across lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,18 @@
+vim.g.lsp_on_attach = function(client, bufnr)
+	local opts = { buffer = bufnr, remap = false }
+
+	vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
+	vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
+	-- vim.keymap.set("n", "<leader>s", function() vim.lsp.buf.workspace_symbol() end, opts)
+	vim.keymap.set("n", "gl", function() vim.diagnostic.open_float() end, opts)
+	vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
+	vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+	vim.keymap.set("n", "<leader>d", "<CMD>Telescope diagnostics<CR>", opts)
+	vim.keymap.set("n", "<leader>a", function() vim.lsp.buf.code_action() end, opts)
+	vim.keymap.set("n", "<leader>R", "<CMD>Telescope lsp_references<CR>", opts)
+	vim.keymap.set("n", "<leader>rn", function() vim.lsp.buf.rename() end, opts)
+	vim.keymap.set("i", "C-f", function() vim.lsp.buf.signature_help() end, opts)
+end
 require('user.settings')
 require('user.plugins')
 require('user.keymaps')
