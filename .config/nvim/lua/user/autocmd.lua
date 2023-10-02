7174958285c1574f6d8bbaf651dd3ae483b13304
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-10-02 10:49:09 +0530

    nvim: make refactoring more generic

--- .config/nvim/lua/user/autocmd.lua
+++ .config/nvim/lua/user/autocmd.lua
@@ -2,7 +2,7 @@ local group = vim.api.nvim_create_augroup("formatting", {})
 
 local function AutoSaveNFormat()
 	if vim.g.__auto_save_n_format then
-		vim.lsp.buf.format({ TabWidth = 4, IndentWidth = 4 })
+		vim.lsp.buf.format({})
 		vim.cmd("update")
 	end
 end
