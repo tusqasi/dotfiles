e5cf9f3d9b755b6d36efaca43855676a414318d9
tusqasi <2002.tushar.kuntawar@gmail.com>
2023-10-02 10:49:38 +0530

    nvim: keymaps

--- .config/nvim/lua/user/keymaps.lua
+++ .config/nvim/lua/user/keymaps.lua
@@ -13,6 +13,15 @@ local SourceAndSyncPlugin = function()
 	require("packer").sync()
 	print("Updated packer plugins!")
 end
+
+local find_nvim_configs = function()
+	local opts = {
+		cwd = "/home/tusqasi/.config/nvim/",
+		find_files = "rg",
+		search_dirs = { "/home/tusqasi/.config/nvim/" },
+	}
+	require("telescope.builtin").find_files(opts)
+end
 local options = { silent = true }
 
 vim.keymap.set("n", "<Leader><Leader>i", SourceAndSyncPlugin, options)
@@ -36,8 +45,8 @@ vim.keymap.set("n", "<Leader>Y", [["+Y]])
 -- Substitute word under cursor
 -- Help c^r
 -- Help c^w
-vim.keymap.set("n", "<Leader><leader>S", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
-vim.keymap.set("n", "<Leader><leader>s", [[:s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<Leader><leader>S", [[:%s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set("n", "<Leader><leader>s", [[:s/<C-r><C-w>/<C-r><C-w>/gI<Left><Left><Left>]])
 
 -- UndotreeToggle
 vim.keymap.set("n", "<Leader>u", "<CMD>UndotreeToggle<CR>")
@@ -61,15 +70,48 @@ vim.keymap.set("n", "<Leader>ff", "<CMD>lua require('telescope.builtin').find_fi
 -- git find
 vim.keymap.set("n", "<Leader>gg", "<CMD>lua require('telescope.builtin').git_files()<CR>", options)
 
--- Find in nvim configs
+-- git find
+vim.keymap.set("n", "<Leader>G", "<CMD>lua require('neogit').open()<CR>", options)
 
+-- Find in nvim configs
+vim.keymap.set("n", "<Leader><Leader>ne", find_nvim_configs, options)
 
 -- Copy whole file
 vim.keymap.set("n", "<Leader>c", [[:norm ggVG"+y<CR>zz]], options)
 
 vim.keymap.set("n", "<Leader>o", AutoSaveNFormatToggle, options)
 
-
--- Telescope document symbols 
+-- Telescope document symbols
 vim.keymap.set("n", "<Leader>s", "<CMD>Telescope lsp_document_symbols<CR>", options)
 
+-- Telescope Live grep
+vim.keymap.set("n", "<Leader>/", "<CMD>Telescope live_grep<CR>", options)
+
+-- Telescope help
+vim.keymap.set("n", "<Leader><Leader>h", "<CMD>Telescope help_tags<CR>", options)
+
+-- Telescope resume
+vim.keymap.set("n", "<Leader><Leader><Leader>", "<CMD>Telescope resume<CR>", options)
+
+-- Harpoon
+vim.keymap.set("n", "<leader>m", "<CMD>lua require('harpoon.mark').add_file()<CR>", options)
+vim.keymap.set("n", "<leader>M", "<CMD>lua require('harpoon.ui').toggle_quick_menu()<CR>", options)
+
+for i = 1, 6, 1 do
+	vim.keymap.set(
+		"n", "<leader>" .. i,
+		string.format("<CMD>lua require('harpoon.ui').nav_file(%s)<CR>", i),
+		options)
+end
+
+-- Git worktree
+vim.keymap.set("n", "<leader>gw", "<CMD>lua  require('telescope').extensions.git_worktree.git_worktrees()<CR>", options)
+vim.keymap.set("n", "<leader>gc", "<CMD>lua require('telescope').extensions.git_worktree.create_git_worktree()<CR>",
+	options)
+
+
+-- Aerial outliner
+-- vim.keymap.set("n", "<leader><leader>o", ":AerialToggle<CR>", options)
+
+-- Git worktree
+vim.keymap.set("n", "<leader>z", "<CMD>:ZenMode<CR>" ,options)
